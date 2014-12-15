235c02672cfdcedb6827704c3070eb8c90cd7017
vagrant <vagrant@rails-dev-box>
2014-12-15 21:57:32 +0000

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EscritorUfm::Application.config.secret_key_base = '856ab2c83c6ebd74167216f2e17345d3c5c78853f00065ca28ae3fe2ab6e335a5a7830d004717eb4c8af2776601adc683d872b1a8f4fc35552b0cace128e67f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EscritorUfm::Application.config.session_store :cookie_store, key: '_escritor-ufm_session'
